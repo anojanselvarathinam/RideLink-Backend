66a424359de1b49b372ac5c50d18fda1472a52f4
anojanselvarathinam <anojanselvaraththinam@gmail.com>
2026-09-22 21:05:12 +0530

    initialize RideLink microservices structure

---
+++ fare-payment-service/src/test/java/com/ridelink/fare_payment_service/FarePaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ridelink.fare_payment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarePaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
